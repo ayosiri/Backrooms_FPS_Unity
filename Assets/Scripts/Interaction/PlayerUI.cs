77df0b8eb88689325b1bf8286c250f935b47d4e2
ayosiri <bandelejasiri8@gmail.com>
2026-04-18 13:26:51 -0400

    Update PlayerUI.cs

--- Assets/Scripts/Interaction/PlayerUI.cs
+++ Assets/Scripts/Interaction/PlayerUI.cs
@@ -3,36 +3,41 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+// Manages on-screen interaction prompts for the player
 public class PlayerUI : MonoBehaviour
 {
     public GameObject interactPrompt;
     public GameObject escapePrompt;
+
     [SerializeField]
     private RawImage promptImage;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
     void Start()
     {
-        
+        // Intentionally left empty (can be used for future initialization if needed)
     }
 
-    // Update is called once per frame
+    // Toggles visibility of the prompt image
     public void UpdateImage(bool showImage)
     {
-       promptImage.enabled = showImage;
+        promptImage.enabled = showImage;
     }
 
+    // Displays the standard interaction prompt
     public void ShowInteractPrompt()
     {
         interactPrompt.SetActive(true);
         escapePrompt.SetActive(false);
     }
 
+    // Displays the escape-specific prompt
     public void ShowEscapePrompt()
     {
         interactPrompt.SetActive(false);
         escapePrompt.SetActive(true);
     }
 
+    // Hides all interaction prompts
     public void HidePrompts()
     {
         interactPrompt.SetActive(false);
